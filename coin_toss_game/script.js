be8e5746aece669c8b23b9b63c95c7ac3ddb3a10
vishrutss <vishrutss@gmail.com>
2023-06-30 20:13:10 +0530

    Added comments to script.js

--- coin_toss_game/script.js
+++ coin_toss_game/script.js
@@ -1,13 +1,21 @@
+// Initialize variables to keep track of the number of heads and tails
 let heads=0;
 let tails=0;
+
+// Get references to the coin, toss button, and reset button elements
 let coin = document.querySelector(".coin");
 let toss = document.querySelector("#toss");
 let reset = document.querySelector("#reset");
 
-// add event listener to toss button
+// Add event listener to toss button
 toss.addEventListener("click", () =>{
+    // Generate a random number (0 or 1) to determine the result of the toss
     let result = Math.floor(Math.random()*2);
+
+    // Stop any ongoing animation on the coin
     coin.style.animation = "none";
+
+    // Update the animation based on the toss result (heads or tails)
     if(result) {
         setTimeout(function() {
             coin.style.animation = "spin-heads 3s forwards";
@@ -19,15 +27,21 @@ toss.addEventListener("click", () =>{
         }, 100);
         tails++;
     }
+
+    // Update the statistics after the animation completes
     setTimeout(updateStats, 3000);
+
+    // Disable the toss button temporarily to prevent rapid clicking
     disableButton();
 });
 
+// Update the displayed statistics (number of heads and tails)
 function updateStats() {
     document.querySelector("#heads").textContent = `Heads: ${heads}`;
     document.querySelector("#tails").textContent = `Tails: ${tails}`;
 }
 
+// Disable the toss button temporarily to prevent rapid clicking
 function disableButton() {
     toss.disabled = true;
     setTimeout(function() {
@@ -35,6 +49,7 @@ function disableButton() {
     }, 3000);
 }
 
+// Reset the coin animation and statistics to their initial state
 reset.addEventListener("click", () => {
     coin.style.animation = "none";
     heads = 0;
