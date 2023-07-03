526980db6dfaae2fbf766f18d0ebe22d6f3cb5e8
vishrutss <vishrutss@gmail.com>
2023-07-03 19:54:56 +0530

    updated script.js

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,20 +1,20 @@
-const container = document.querySelector(".container");
-const search = document.querySelector(".search-box button");
-const weatherBox = document.querySelector(".weather-box");
-const weatherDetails = document.querySelector(".weather-details");
-const error404 = document.querySelector(".not-found");
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
-search.addEventListener("click", () => {
+search.addEventListener('click', () => {
     const APIKey = process.env.API_KEY;
-    const city = document.querySelector(".search-box input").value;
+    const city = document.querySelector('.search-box input').value;
 
-    if (city === "") return;
+    if (city === '') return;
 
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
     )
-        .then((response) => response.json())
-        .then((data) => {
+        .then(response => response.json())
+        .then(data => {
             if (data.cod ==='404') {
                 container.computedStyleMap.height = '400px';
                 weatherBox.style.display = 'none';
@@ -33,7 +33,7 @@ search.addEventListener("click", () => {
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
-            switch (json.weather[0].main) {
+            switch (data.weather[0].main) {
                 case 'Clear':
                     image.src = 'images/clear.png';
                     break;
@@ -58,10 +58,10 @@ search.addEventListener("click", () => {
                     image.src = '';
             }
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            temperature.innerHTML = `${parseInt(data.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${data.weather[0].description}`;
+            humidity.innerHTML = `${data.main.humidity}%`;
+            wind.innerHTML = `${parseInt(data.wind.speed)}Km/h`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
