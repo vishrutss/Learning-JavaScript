e48fd57d1e893579eb0d7d9310bc651f40b0230c
vishrutss <vishrutss@gmail.com>
2023-07-05 18:17:03 +0530

    Updated script.js

---
+++ type_speed_test/script.js
@@ -0,0 +1,96 @@
+//Random quotes api
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
+
+let quote = "";
+let time = 60;
+let timer = "";
+let mistakes = 0;
+
+const renderNewQuote = async () => {
+    const response = await fetch(quoteApiUrl);
+    let data = await response.json();
+    quote = data.content;
+
+    let arr=quote.split("").map((value) => {
+        return "<span class='quote-chars'>" + value + "</span>";
+    });
+    quoteSection.innerHTML = arr.join("");
+};
+
+userInput.addEventListener("input", () => {
+    let quoteChars = document.querySelectorAll(".quote-chars");
+    quoteChars = Array.from(quoteChars);
+
+    let userInputChars = userInput.value.split("");
+    quoteChars.forEach((charSpan, index) => {
+        if(charSpan.innerText == userInputChars[index]) {
+            charSpan.classList.add("correct");
+        } else if (userInputChars[index] == null) {
+            if (char.classList.contains("correct")) {
+                charSpan.classList.remove("correct");
+            } else {
+                char.classList.remove("incorrect");
+            }
+        } else {
+            if (!char.classList.contains("incorrect")) {
+                mistakes++;
+                char.classList.add("incorrect");
+            }
+            document.getElementById("mistakes").innerText = mistakes;
+        }
+
+        let check = quoteChars.every((element) => {
+            return element.classList.contains("correct");
+        });
+
+        if(check) {
+            displayResult();
+        }
+    });
+});
+
+function updateTimer() {
+    if(time == 0) {
+        displayResult();
+    } else {
+        document.getElementById("timer").innerText = --time + "s";
+    }
+}
+
+const timeReduce = () => {
+    time = 60;
+    timer = setInterval(updateTimer, 1000);
+};
+
+const displayResult = () => {
+    document.querySelector(".result").style.display = "block";
+    clearInterval(timer);
+    document.getElementById("stop-test").style.display = "none";
+    userInput.disabled = true;
+    let timeTaken = 1;
+    if(time != 0) {
+        timeTaken = (60 - time) / 100;
+    }
+    document.getElementById("wpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
+    document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100) + "%";
+};
+
+//Start test
+const startTest = () => {
+    mistakes = 0;
+    timer = "";
+    userInput.disabled = false;
+    timeReduce();
+    document.getElementById("start-test").style.display = "none";
+    document.getElementById("stop-test").style.display = "block";
+};
+
+window.onload = () => {
+    userInput.value = "";
+    document.getElementById("start-test").style.display = "block";
+    document.getElementById("stop-test").style.display = "none";
+    userInput.disabled = true;
+    renderNewQuote();
+}
