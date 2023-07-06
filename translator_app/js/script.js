5a863a5474b7d15e8dce546adbb945cad4df2a67
vishrutss <vishrutss@gmail.com>
2023-07-06 19:11:17 +0530

    Added fix and comments to script.js

--- translator_app/js/script.js
+++ translator_app/js/script.js
@@ -5,6 +5,7 @@ const selectTag = document.querySelectorAll("select");
 const icons = document.querySelectorAll(".row i");
 const translateBtn = document.querySelector("button");
 
+// Populate select tags with options for language selection
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
         let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "am-ET" ? "selected" : "";
@@ -13,6 +14,7 @@ selectTag.forEach((tag, id) => {
     }
 });
 
+// Exchange input language and translated language
 exchageIcon.addEventListener("click", () => {
     let tempText = fromText.value, tempLang = selectTag[0].value;
     fromText.value = toText.value;
@@ -21,6 +23,7 @@ exchageIcon.addEventListener("click", () => {
     selectTag[1].value = tempLang;
 });
 
+// Clear "to" text if "from" text is empty
 fromText.addEventListener("keyup", () => {
     if (!fromText.value) toText.value = "";
 });
@@ -29,15 +32,22 @@ translateBtn.addEventListener("click", () => {
     let text = fromText.value.trim(), translateFrom = selectTag[0].value, translateTo = selectTag[1].value;
     if(!text)
         return;
+
+    // Set placeholder while translating
     toText.setAttribute("placeholder", "Translating...");
-    let api = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${translateFrom}&tl=${translateTo}&dt=t&q=${encodeURI(text)}`;
+    let apiUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${translateFrom}&tl=${translateTo}&dt=t&q=${encodeURI(text)}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
-        toText.value=data.responseData.translatedText;
+        // Display translated text
+        toText.value=data[0][0][0];
+
+        // If available, display alternative translation
         data.matches.forEach(data => {
             if(data.id===0){
                 toText.value=data.translation;
             }
         });
+
+        // Set placeholder back to normal
         toText.setAttribute("placeholder", "Translated");
     });
 });
@@ -46,12 +56,14 @@ icons.forEach(icon => {
     icon.addEventListener("click", ({ target }) => {
         if (!fromText.value || !toText.value) return;
         if (target.classList.contains("fa-copy")) {
+            // Copy text to clipboard
             if (target.id == "from") {
                 navigator.clipboard.writeText(fromText.value);
             } else {
                 navigator.clipboard.writeText(toText.value);
             }
         } else {
+            // Speak the text using text-to-speech
             let utterance;
             if (target.id == "from") {
                 utterance = new SpeechSynthesisUtterance(fromText.value);
